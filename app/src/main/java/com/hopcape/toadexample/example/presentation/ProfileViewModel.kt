e368878194f0d9cb75c38d9192d3e947d8a4ae02
the-mobile-architect <komputersphere@gmail.com>
2026-01-04 01:26:55 +0530

    Add initial actions to the viewModel

--- app/src/main/java/com/hopcape/toadexample/example/presentation/ProfileViewModel.kt
+++ app/src/main/java/com/hopcape/toadexample/example/presentation/ProfileViewModel.kt
@@ -7,7 +7,8 @@ import com.hopcape.toadexample.example.data.ProfileRepository
 
 class ProfileViewModel(
     private val repository: ProfileRepository,
-    private val preferencesRepository: PreferencesRepository
+    private val preferencesRepository: PreferencesRepository,
+    initialActions: List<ProfileAction> = emptyList()
 ): ToadViewModel<ProfileUiState, ProfileEvent>(ProfileUiState.Loading){
     override val dependencies = ProfileActionDependencies(
         profileRepository = repository,
@@ -15,5 +16,8 @@ class ProfileViewModel(
         coroutineScope = viewModelScope
     )
 
+    init {
+        dispatchAll(initialActions)
+    }
     fun runAction(action: ProfileAction) = dispatch(action)
 }
